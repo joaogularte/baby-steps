827c95eb4e387f61c4a8385095916d7e4eaee2cb
joaogularte <jov96@hotmail.com>
2019-11-17 21:55:15 -0300

    examplos com key-value list e maps

---
+++ keywordlists_maps.exs
@@ -0,0 +1,29 @@
+#list - key deve ser um atom
+#     - Keys devem ser ordenadas, pelo dev
+#     - Keys podem ser repetidas
+# geralmente usadas para passar valores opcionais 
+
+list = [a: "oio", b: "foi", a: "oia"]
+IO.puts list[:a]
+
+list = [{:a, 1}, {:b, 3}]
+IO.inspect list == [a: 1, b: 3]
+
+
+#maps - key-value store preferivel
+#     - permite qualquer valor como key
+#     - nao segui nenhum ordem
+
+map = %{:a =>div(12,3), 2 => "foi"}
+IO.inspect map[2]
+new_map = Map.put(map, :ato, 3)
+another_map = %{new_map | 2 => 35}
+IO.inspect another_map
+
+#Nested data Structures
+users = [
+  john: %{name: "John", age: 27, languages: ["Erlang", "Ruby", "Elixir"]},
+  mary: %{name: "Mary", age: 29, languages: ["Elixir", "F#", "Clojure"]}
+]
+
+IO.puts users[:john].name
