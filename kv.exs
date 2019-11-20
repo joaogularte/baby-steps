132eaefb73ffa59808a1fdba3de659d9d6d7684c
joaogularte <joaogularte@u149326.unisc.br>
2019-11-20 09:27:21 -0300

    Exemplo sobre processes e state

--- kv.exs
+++ kv.exs
@@ -5,7 +5,9 @@ defmodule KV do
 
     defp loop(map) do
         receive do
-            {:get, key, caller} -> Map.get(map, key)
+            {:get, key, caller} -> 
+                send caller, Map.get(map, key)
+                loop(map)
             {:put, key, value} -> loop(Map.put(map, key, value))
         end
     end
