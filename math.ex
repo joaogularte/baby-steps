65d4f850fda59b784cd358412750028834755065
joaogularte <jov96@hotmail.com>
2019-11-27 22:54:53 -0300

    Adicionado atributo

--- math.ex
+++ math.ex
@@ -1,5 +1,17 @@
 defmodule Math do
+    @moduledoc """
+    
+
+
+    """
+
+    @doc """
+    
+    """
+
+    @initial_state %{host: "127.0.0.1", port: 8080}
     def sum(a, b) do
+        IO.inspect @initial_state
         a+b
     end
 end
