e75e730905e884bb567882f8684e51fdd0262470
joaogularte <jov96@hotmail.com>
2020-03-02 20:08:45 -0300

    comentarios sobre require, import e use

---
+++ require_import_use.exs
@@ -0,0 +1,8 @@
+#import - importa somente funções de um modulo para outro modulo
+import Foo
+
+#require - requisita um modulo para usar suas macros
+require Foo
+
+#use - como uma extensão, permite que um modulo injete codigo em outro modulo
+use Foo
