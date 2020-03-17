3f5fe26a1c54a87add8a2f4644f632da824f6df2
joaogularte <jov96@hotmail.com>
2020-03-17 07:56:30 -0300

    Anotações sobre genserver

---
+++ genserver.exs
@@ -0,0 +1,5 @@
+# call -> sincrono. Server é obrigado a enviar uma resposta para cada request.
+# enquanto o server processa o respose, o client fica aguardando
+
+# cast -> assincrono. Server não envia devolta um response e portanto,
+# o cliente não espera por um.
